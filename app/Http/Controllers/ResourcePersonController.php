33ff134763fc831c7e3af9ea1a15aa6c18fab269
nsol hub <44431757+nsolhub@users.noreply.github.com>
2019-03-01 15:25:49 +0500

    Add files via upload
    upload cv added

--- app/Http/Controllers/ResourcePersonController.php
+++ app/Http/Controllers/ResourcePersonController.php
@@ -18,27 +18,37 @@ class ResourcePersonController extends Controller
 
 	public function create(Request $request)
    	{
+         
    		$userid = Auth::user()->id;
    		$user_email = Auth::user()->email;
    		$person = new ResourcePerson;
    		$validated = $request->validate([	
    			"firstname" => 'required|max:25',
    			"lastname" => 'required|max:25',
-   			"cnic" => 'unique:resource_people|required|digits:13',
+   			"cnic" => 'unique:resource_people|required|digits:14',
    			"designation" => 'max:100',
    			"gender" => 'max:10',
-   			"cell_number" => 'digits:12',
+   			"cell_number" => 'digits:11',
    			"phone" => 'max:15',
    			"email" => 'required|email',
+            "cv" => 'required'
    			
    		]);
+         if ($request->hasFile('cv')) { 
+            $cv = $request->file('cv'); 
+            $name = time().'_'.$cv->getClientOriginalName(); 
+            $destinationPath = public_path().'/front/images'; 
+            $upload = $cv->move($destinationPath, $name); 
+         }
+
    		$person->first_name= $request->firstname;
    		$person->last_name= $request->lastname;
    		$person->cnic= $request->cnic;
          $person->vendor_no= $request->vendor_no;
    		$person->bank= $request->bank;
    		$person->cell_number= $request->cell_number;
-   		$person->email= $request->email;
+         $person->email= $request->email;
+   		$person->cv= $name;
    		$person->save();
 
    		Session::flash('message', 'Resource Person Data Successfull Added. <script> swal("Added", "You have added a new Resource Person!", "success"); </script> '); 
